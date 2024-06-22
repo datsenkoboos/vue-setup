0b1fa95d17ab3a1c9312ae1e5e7eab1c17d432f1
Arthur Datsenko-Boos <adacenkoboos@gmail.com>
2024-06-22 14:41:35 +0700

    add localStorage mock

--- setupTests.ts
+++ setupTests.ts
@@ -1,4 +1,11 @@
-import { expect } from 'vitest';
+import { beforeAll, expect } from 'vitest';
 import * as matchers from '@testing-library/jest-dom/matchers';
+import { localStorageMock } from './src/mocks';
 
 expect.extend(matchers);
+
+beforeAll(() => {
+  Object.defineProperty(window, 'localStorage', {
+    value: localStorageMock,
+  });
+});
