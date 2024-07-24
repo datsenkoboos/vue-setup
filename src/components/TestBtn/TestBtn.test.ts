bf8e1faf75dcbce615e97afd00b999b2627e58bc
Arthur Datsenko-Boos <88643277+datsenkoboos@users.noreply.github.com>
2024-07-24 08:29:13 +0300

    Update readme (#22)

--- src/components/TestBtn/TestBtn.test.ts
+++ src/components/TestBtn/TestBtn.test.ts
@@ -1,6 +1,7 @@
 import TestBtn from './TestBtn.vue';
 import { describe, test, expect, beforeEach } from 'vitest';
 import { cleanup, render, screen } from '@testing-library/vue';
+import { faker } from '@faker-js/faker';
 
 describe('TestBtn', () => {
   beforeEach(() => {
@@ -17,7 +18,7 @@ describe('TestBtn', () => {
   };
 
   test('should render slot content', () => {
-    const testSlotContent = 'test slot content';
+    const testSlotContent = faker.string.uuid();
     mount(testSlotContent);
 
     const testBtn = screen.getByRole('button');
