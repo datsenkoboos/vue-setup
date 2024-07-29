52e81833cd41adba361ceddeb1fd23feb054181b
datsenkoboos <adacenkoboos@gmail.com>
2024-07-29 19:37:14 +0700

    add basic 404 page

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -2,6 +2,6 @@
 
 <template>
   <div class="h-full grid place-items-center">
-    <h1 class="text-xl font-semibold">Index</h1>
+    <h1 class="text-6xl font-semibold">Index</h1>
   </div>
 </template>
