e86a468b45c13c3f72697c5466852b720a5df3fa
Arthur Datsenko-Boos <adacenkoboos@gmail.com>
2024-09-17 13:09:00 +0700

    remove example of https certificates config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-// import * as fs from 'fs';
 import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
@@ -6,20 +5,9 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    // https: {
-    //   key: fs.readFileSync('../../localhost-key.pem'),
-    //   cert: fs.readFileSync('../../localhost.pem'),
-    // },
-  },
   preview: {
     port: 5173,
     strictPort: true,
-    // https:
-    //   {
-    //     key: fs.readFileSync('../../localhost-key.pem'),
-    //     cert: fs.readFileSync('../../localhost.pem'),
-    //   },
   },
   plugins: [vue()],
   resolve: {
