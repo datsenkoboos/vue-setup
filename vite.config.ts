2fcd05eff9f2d2ac72b471c61f16053fe4e3f8bb
Arthur Datsenko-Boos <88643277+datsenkoboos@users.noreply.github.com>
2025-02-18 19:33:47 +0700

    Tailwind V4 (#58)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
   preview: {
     port: 5173,
     strictPort: true,
