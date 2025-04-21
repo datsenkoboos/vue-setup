c31d3669e3deb84b5e80d1154067b048736e8c0a
Arthur Datsenko-Boos <adacenkoboos@gmail.com>
2025-04-21 13:14:04 +0700

    update regression tests

---
+++ .storybook-regression/preview.ts
@@ -0,0 +1,35 @@
+import { type Preview, setup } from '@storybook/vue3';
+import { VueQueryPlugin } from '@tanstack/vue-query';
+import MockDate from 'mockdate';
+
+import './compiled.css';
+
+import '@/fonts';
+import { createPinia } from 'pinia';
+import { vueRouter } from 'storybook-vue3-router';
+import { type App } from 'vue';
+
+import { routes } from '../src/router';
+
+MockDate.set('2025-04-21 14:12:46');
+
+const pinia = createPinia();
+
+setup((app: App) => {
+  app.use(pinia);
+  app.use(VueQueryPlugin);
+});
+
+const preview: Preview = {
+  decorators: [vueRouter(routes)],
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
